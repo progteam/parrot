097a625f05cde75b6d1adc71771ebf0c04b6b2a3
Donald Dong <mail@ddong.me>
2019-02-17 04:31:15 +0000

    Cleanup initial files
    When we created the frontend using `create-react-app`, we have checked
    in the starter files (or code). So we want to remove some of them which
    are unused.
    
    Note `/package.json` was checked in by me unintentionally. It was not
    created by `create-react-app`.
    
    PR: #7

--- frontend/watch.js
+++ frontend/watch.js
@@ -2,8 +2,6 @@
 //  https://gist.github.com/int128/e0cdec598c5b3db728ff35758abdbafd 
 process.env.NODE_ENV = 'development';
 
-const fs = require('fs-extra');
-const paths = require('react-scripts/config/paths');
 const webpack = require('webpack');
 const configFactory = require('react-scripts/config/webpack.config');
 const config = configFactory('development');
