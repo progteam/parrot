7cd45d05900739dc43f28fd45af09293089bd1e2
rafael123rr <30412149+rafael123rr@users.noreply.github.com>
2019-04-12 15:20:45 -0700

    Updates to the scoreboard for day/week/month
    Created a day/week/month function which all call back to a general
    function. This general function takes in the number of days to go back
    based on the caller function and all 3 functions return a JSON blob.
    
    PR: #52

--- backend/scoreboard/views.py
+++ backend/scoreboard/views.py
@@ -10,21 +10,43 @@ from django.http import JsonResponse
 from django.db.models import Max, Min
 from .models import KattisScore
 
-def scores_week(request):
+def get_scores(days):
     """
-    Returns the score difference for each KattisHandle
-    for the past week.
+    Return a Json Response with a list of scores which are the diffence
+    between our current date and the argument days.
     """
-    week_ago = datetime.now(
-        tz=get_current_timezone()) - timedelta(days=7)
+
+    days_ago = datetime.now(tz=get_current_timezone()) - timedelta(days=days)
     query_result = KattisScore.objects.filter(
-        created_at__gte=week_ago
+        created_at__gte=days_ago
     ).values('kattis_handle__handle').annotate(
         points=Max('score') - Min('score')
     )
+
     score_diff_list = map(
         lambda obj: {
             "username" : obj['kattis_handle__handle'],
             "points" : obj['points'],
         }, query_result)
     return JsonResponse(list(score_diff_list), safe=False)
+
+def scores_day(request):
+    """
+    Returns the score difference for each KattisHandle
+    for the past day.
+    """
+    return get_scores(days=1)
+
+def scores_week(request):
+    """
+    Returns the score difference for each KattisHandle
+    for the past week.
+    """
+    return get_scores(days=7)
+
+def scores_month(request):
+    """
+    Returns the score difference for each KattisHandle
+    for the past month.
+    """
+    return get_scores(days=30)
