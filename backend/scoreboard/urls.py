0f18a0486bd96a4f2501d533a3d7fa2c2533642f
rafael123rr <rrivas@csumb.edu>
2019-04-12 14:30:52 -0700

    Updates to the scoreboard for day/week/month
    Created a day/week/month function which all call back to a general
    function. This general function takes in the amount of days to go back
    based on the caller function and all 3 functions return a JSON blob.
    
    PR: #52

--- backend/scoreboard/urls.py
+++ backend/scoreboard/urls.py
@@ -5,9 +5,11 @@ The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
 """
 from django.urls import path
-from .views import scores_week
+from .views import scores_day, scores_week, scores_month
 
 # pylint: disable=invalid-name
 urlpatterns = [
-    path('week', scores_week)
+    path('day', scores_day),
+    path('week', scores_week),
+    path('month', scores_month)
 ]
